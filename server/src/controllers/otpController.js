694c2809b811884f2af692d78c804250997073c0
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2024-11-07 02:00:49 +0800

    otp controller added

--- server/src/controllers/otpController.js
+++ server/src/controllers/otpController.js
@@ -2,12 +2,10 @@ const { sendOTP } = require("../utils/sendOTP");
 const otpModel = require("../models/otpModel");
 const userModel = require("../models/userModel");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { Sequelize } = require("sequelize");
+const sequelize = require("../config/database");
 require("dotenv").config();
 const { setTokens } = require("../helpers/tokenHelpers");
 const statusList = require("../constants/statusList");
-const sequelize = require("../configs/database");
 const date = require("date-and-time");
 
 const postOTP = async (email) => {
@@ -51,64 +49,41 @@ const verifyOTP = async (req, res) => {
         .json({ message: "Invalid OTP. Please try again." });
     }
 
-    // if (expiresAt < Date.now()) {
-    //   console.log(expiresAt, Date.now());
-    //   return res
-    //     .status(400)
-    //     .json({ message: "OTP expired. Pleae request a new OTP." });
-    // }
-
     if (new Date(expiresAt).getTime() < Date.now()) {
-      console.log(new Date(expiresAt).getTime(), Date.now());
       return res.status(400).json({
-        message: `${new Date(
-          expiresAt
-        )} OTP expired. Please request a new OTP.`,
+        message: "OTP expired. Please request a new OTP.",
       });
     }
+
+    const registeredUser = await userModel.findOne({
+      where: { email: email, status: statusList.verified },
+    });
 
     // initialize access token
     let accessToken = null;
 
-    await userModel.update(
-      {
-        status: statusList.verified,
-        updatedAt: sequelize.literal(`'${formattedDate}'`),
-      },
-      { where: { email: email } }
-    );
-
-    //  generate tokens
-    const tokens = setTokens(res, { email, userRole });
-    accessToken = tokens.accessToken;
-
-    //   accessToken = jwt.sign({ email, userRole }, process.env.ACCESS_TOKEN, {
-    //     expiresIn: "30m",
-    //   });
-    //   const refreshToken = jwt.sign(
-    //     { email, userRole },
-    //     process.env.REFRESH_TOKEN,
-    //     {
-    //       expiresIn: "60m",
-    //     }
-    //   );
-
-    //   // Set secure HTTP-only cookies
-    //   res.cookie("accessToken", accessToken, {
-    //     httpOnly: true,
-    //     maxAge: 30 * 60 * 1000,
-    //   }); // 30 minutes
-    //   res.cookie("refreshToken", refreshToken, {
-    //     httpOnly: true,
-    //     maxAge: 60 * 60 * 1000,
-    //   }); // 30 minutes
+    if (!registeredUser) {
+      await userModel.update(
+        {
+          status: statusList.verified,
+          updatedAt: sequelize.literal(`'${formattedDate}'`),
+        },
+        { where: { email: email } }
+      );
+    } else {
+      //  generate tokens
+      const tokens = setTokens(res, { email, userRole });
+      accessToken = tokens.accessToken;
+    }
 
     // Delete the OTP after successful verification
     await otpModel.destroy({ where: { email: email } });
 
     return res.status(200).json({
       status: "success",
-      message: "Registration Successful",
+      message: registeredUser
+        ? "Registration Successful"
+        : "Registration Successful. Please login your account",
       role: userRole,
       accessToken: accessToken,
     });
@@ -153,6 +128,9 @@ const verifyChangeEmail = async (req, res) => {
   const { id } = req.params;
   const { email, otp } = req.body;
 
+  const createdAt = new Date();
+  const formattedDate = date.format(createdAt, "YYYY-MM-DD HH:mm:ss", true); // true for UTC time;
+
   try {
     const matchedOTPRecord = await otpModel.findOne({
       where: { email: email },
@@ -178,7 +156,7 @@ const verifyChangeEmail = async (req, res) => {
     await userModel.update(
       {
         email: email,
-        updatedAt: createdAt,
+        updatedAt: sequelize.literal(`'${formattedDate}'`),
       },
       { where: { id: id } }
     );

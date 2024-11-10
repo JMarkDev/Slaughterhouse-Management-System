69ff8d1cdb089284b4b3ec289d20cbc156b0f7d0
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2024-11-11 01:27:20 +0800

    pagination added

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,84 +1,9 @@
-// import PropTypes from "prop-types";
-
-// const Pagination = ({
-//   documentsPerPage,
-//   totalDocuments,
-//   paginate,
-//   currentPage,
-// }) => {
-//   const pageNumbers = [];
-
-//   for (let i = 1; i <= Math.ceil(totalDocuments / documentsPerPage); i++) {
-//     pageNumbers.push(i);
-//   }
-
-//   return (
-//     <nav aria-label="Page navigation example">
-//       <ul className="inline-flex -space-x-px text-sm">
-//         <li>
-//           <button
-//             onClick={() => paginate(currentPage - 1)}
-//             disabled={currentPage === 1}
-//             className={`px-3 h-8 leading-tight ${
-//               currentPage === 1
-//                 ? "text-gray-300 cursor-not-allowed"
-//                 : "text-gray-500"
-//             } bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
-//           >
-//             Previous
-//           </button>
-//         </li>
-//         {pageNumbers.map((number) => (
-//           <li key={number}>
-//             <button
-//               onClick={() => paginate(number)}
-//               className={`px-3 h-8 leading-tight ${
-//                 currentPage === number
-//                   ? "text-blue-600 bg-blue-200 border-blue-300"
-//                   : "text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-//               }`}
-//             >
-//               {number}
-//             </button>
-//           </li>
-//         ))}
-//         <li>
-//           <button
-//             onClick={() => paginate(currentPage + 1)}
-//             disabled={currentPage === pageNumbers.length}
-//             className={`px-3 h-8 leading-tight ${
-//               currentPage === pageNumbers.length
-//                 ? "text-gray-300 cursor-not-allowed"
-//                 : "text-gray-500"
-//             } bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
-//           >
-//             Next
-//           </button>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// Pagination.propTypes = {
-//   documentsPerPage: PropTypes.number,
-//   totalDocuments: PropTypes.number,
-//   paginate: PropTypes.func,
-//   currentPage: PropTypes.number,
-// };
-
-// export default Pagination;
 import PropTypes from "prop-types";
 
-const Pagination = ({
-  documentsPerPage,
-  totalDocuments,
-  paginate,
-  currentPage,
-}) => {
+const Pagination = ({ dataPerPage, totalData, paginate, currentPage }) => {
   const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalDocuments / documentsPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(totalData / dataPerPage); i++) {
     pageNumbers.push(i);
   }
 
@@ -131,8 +56,8 @@ const Pagination = ({
 };
 
 Pagination.propTypes = {
-  documentsPerPage: PropTypes.number,
-  totalDocuments: PropTypes.number,
+  dataPerPage: PropTypes.number,
+  totalData: PropTypes.number,
   paginate: PropTypes.func,
   currentPage: PropTypes.number,
 };

9bbb32885004a33b530e740f329a86797e856b72
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2024-11-25 01:27:53 +0800

    admin slaughterhouse records done

---
+++ client/src/pages/Admin/SlaughterhouseRecords/Records.jsx
@@ -0,0 +1,171 @@
+import { useEffect, useState, useRef } from "react";
+import { IoSearch } from "react-icons/io5";
+import { useReactToPrint } from "react-to-print";
+import { useParams } from "react-router-dom";
+import {
+  getFilteredAnimals,
+  filterAllAnimals,
+} from "../../../services/animalsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import TransactionTable from "../../../components/table/TransactionTable";
+import Pagination from "../../../components/Pagination";
+import { getUserData } from "../../../services/authSlice";
+import Dropdown from "../../../components/dropdown/Dropdown";
+import AnimalDropdown from "../../../components/dropdown/AnimalsDropdown";
+import transactionStatus from "../../../constants/transactionStatus";
+import PrintAll from "../Transaction/PrintAll";
+
+const SlaughterTransaction = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(getUserData);
+  const { id } = useParams();
+  const animals = useSelector(getFilteredAnimals);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [animalsList, setAnimalsList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [animalType, setAnimalType] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [status, setStatus] = useState(null);
+  const [unpaidList, setUnpaidList] = useState([]);
+  const contentRef = useRef(null);
+
+  const dataPerPage = 5;
+  useEffect(() => {
+    dispatch(
+      filterAllAnimals({
+        type: animalType,
+        startDate: startDate,
+        endDate: endDate,
+        slaughterhouseId: id,
+        transactionID: searchTerm,
+        status: status,
+      })
+    );
+  }, [dispatch, animalType, startDate, endDate, user, searchTerm, status, id]);
+
+  useEffect(() => {
+    if (animals) {
+      setAnimalsList(animals);
+    }
+  }, [animals]);
+
+  const fetchUpdate = () => {
+    dispatch(
+      filterAllAnimals({
+        type: animalType,
+        startDate: startDate,
+        endDate: endDate,
+        slaughterhouseId: "All",
+        transactionID: searchTerm,
+        status: status,
+      })
+    );
+  };
+
+  const handleFilter = (status) => {
+    setStatus(status);
+  };
+
+  const handleFilterByAnimal = (animal) => {
+    if (animal === "Default" || animal === "All") {
+      setAnimalType("All");
+    } else {
+      setAnimalType(animal);
+    }
+  };
+
+  // Paganation
+  const indexOfLastDocument = currentPage * dataPerPage;
+  const indexOfFirstDocument = indexOfLastDocument - dataPerPage;
+  const currentData = animalsList?.slice(
+    indexOfFirstDocument,
+    indexOfLastDocument
+  );
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  useEffect(() => {
+    if (animals) {
+      setAnimalsList(animals);
+
+      // Filter unpaid transactions
+      const unpaid = animals.filter(
+        (animal) =>
+          animal.transaction.status === transactionStatus.unpaid ||
+          animal.transaction.status === transactionStatus.partial
+      );
+      setUnpaidList(unpaid);
+    }
+  }, [animals]);
+
+  const handleReactToPrint = useReactToPrint({
+    contentRef,
+    documentTitle: "Demand Letter",
+    onAfterPrint: () => console.log("Printing completed"),
+    onPrintError: (errorLocation, error) =>
+      console.error("Error:", errorLocation, error),
+  });
+
+  const handlePrintAll = () => {
+    const unpaidTransactions = animalsList.filter(
+      (animal) =>
+        animal.transaction.status === transactionStatus.unpaid ||
+        animal.transaction.status === transactionStatus.partial
+    );
+
+    if (unpaidTransactions.length > 0) {
+      setTimeout(() => {
+        handleReactToPrint();
+      }, 500);
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex justify-end items-end gap-5  flex-col">
+        <div className=" flex lg:flex-row flex-col justify-end w-full items-end relative gap-3">
+          <div className=" flex max-w-[450px] w-full  items-center relative">
+            <input
+              type="text"
+              placeholder="Search Transaction ID"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="border border-blue-500 focus:border-blue  rounded-xl w-full bg-gray-100 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            />
+            <IoSearch className="text-2xl absolute right-2 text-gray-600" />
+          </div>
+
+          <div className="flex justify-center items-center gap-3">
+            <AnimalDropdown handleFilter={handleFilterByAnimal} />
+            <Dropdown handleFilter={handleFilter} />
+          </div>
+          <button
+            onClick={handlePrintAll}
+            className="bg-blue-600 text-nowrap hover:bg-blue-700 text-white font-bold text-sm p-2 px-4 rounded-lg"
+          >
+            Print All Demand Letter
+          </button>
+        </div>
+      </div>
+      <div className="mt-8">
+        <TransactionTable animalsList={currentData} fetchUpdate={fetchUpdate} />
+        <div className="flex justify-end mt-5">
+          <Pagination
+            dataPerPage={dataPerPage}
+            totalData={animalsList?.length}
+            paginate={paginate}
+            currentPage={currentPage}
+          />
+        </div>
+      </div>
+      <div style={{ display: "none" }}>
+        {" "}
+        {/* Hidden offscreen */}
+        <PrintAll transactions={unpaidList} contentRef={contentRef} />
+      </div>
+    </div>
+  );
+};
+
+export default SlaughterTransaction;
--- client/src/pages/Admin/SlaughterhouseRecords/SlaughterhouseRecords.jsx
+++ client/src/pages/Admin/SlaughterhouseRecords/SlaughterhouseRecords.jsx
@@ -1,5 +1,71 @@
-const SlaughterhouseRecords = () => {
-  return <div>SlaughterhouseRecords</div>;
+import { useEffect, useState } from "react";
+import { IoSearch } from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
+import Pagination from "../../../components/Pagination";
+import {
+  getAllUsers,
+  fetchUsers,
+  searchSlaughterhouseRole,
+} from "../../../services/usersSlice";
+import SlaughterhouseTable from "../../../components/table/SlaughterhouseTable";
+import rolesList from "../../../constants/rolesList";
+
+const Slaughterhouse = () => {
+  const dispatch = useDispatch();
+  const supervisor = useSelector(getAllUsers);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const dataPerPage = 5;
+
+  const fetchUpdate = () => {
+    dispatch(fetchUsers(rolesList?.supervisor));
+  };
+
+  useEffect(() => {
+    if (searchTerm) {
+      dispatch(searchSlaughterhouseRole(searchTerm));
+    } else {
+      dispatch(fetchUsers(rolesList?.supervisor));
+    }
+  }, [searchTerm, dispatch]);
+
+  // Paganation
+  const indexOfLastDocument = currentPage * dataPerPage;
+  const indexOfFirstDocument = indexOfLastDocument - dataPerPage;
+  const currentData = supervisor?.slice(
+    indexOfFirstDocument,
+    indexOfLastDocument
+  );
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div>
+      <div className="flex text-sm md:text-[16px] justify-end gap-5">
+        <div className=" flex max-w-[450px] w-full items-center relative">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="border border-blue-500 focus:border-blue  rounded-xl w-full bg-gray-100 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+          <IoSearch className="text-2xl absolute right-2 text-gray-600" />
+        </div>
+      </div>
+      <div className="mt-8">
+        <SlaughterhouseTable users={currentData} fetchUpdate={fetchUpdate} />
+        <div className="flex justify-end mt-5">
+          <Pagination
+            dataPerPage={dataPerPage}
+            totalData={currentData?.length}
+            paginate={paginate}
+            currentPage={currentPage}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default SlaughterhouseRecords;
+export default Slaughterhouse;
